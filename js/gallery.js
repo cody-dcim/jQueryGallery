98869daaff5b91da03338ef8c06fe233d97034a7
Cody Lettau <lettau@dcim.com>
2014-05-23 15:03:50 -0500

    Cleaned up plugin source a bit

--- js/gallery.js
+++ js/gallery.js
@@ -1,137 +1,141 @@
-var tId,
-	THUMBNAIL_WIDTH = 140;
-
-var slideNext = function() {
-	
-    var active = $('#slideshow .img-wrapper.active');
-
-    if (active.length === 0) { active = $('#slideshow .img-wrapper:last'); }
-
-    active.addClass('last-active');
-
-    // Setting next image & thumb properties
-    loadNextImage(active);
-};
-
-var slidePrev = function() {
-    var active = $('#slideshow .img-wrapper.active');
-
-    if (active.length === 0) { active = $('#slideshow .img-wrapper:last'); }
-
-    active.addClass('last-active');
-
-    // Setting next image & thumb properties
-    loadPrevImage(active);
-};
-
-var loadNextImage = function(active) {
-	var next =  active.next(".img-wrapper").length ? active.next(".img-wrapper") : $('#slideshow .img-wrapper:first'),
-		nextThumb = $('[data-thumb-id="' + next.data('img-id') + '"]');
-
-	// Setting next image & thumb properties
-	$('.thumb').removeClass('active');
-    nextThumb.addClass('active');
-    scrollThumbnails(nextThumb);
-    next.css({opacity: 0.0})
-        .addClass('active')
-        .animate({opacity: 1.0}, 1000, function() {
-            active.removeClass('active last-active');
-        });
-};
-
-var loadPrevImage = function(active) {
-	var prev =  active.prev(".img-wrapper").length ? active.prev(".img-wrapper") : $('#slideshow .img-wrapper:last'),
-		prevThumb = $('[data-thumb-id="' + prev.data('img-id') + '"]');
-
-	// Setting next image & thumb properties
-	$('.thumb').removeClass('active');
-	prevThumb.addClass('active');
-	scrollThumbnails(prevThumb);
-    prev.css({opacity: 0.0})
-        .addClass('active')
-        .animate({opacity: 1.0}, 1000, function() {
-            active.removeClass('active last-active');
-        });
-};
-
-var loadClickedImage = function(id) {
-	var image =  $('[data-img-id="' + id + '"]'),
-		imgThumb = $('[data-thumb-id="' + id + '"]'),
-		currActive = $('.img-wrapper.active');
-
-	// Setting next image & thumb properties
-	currActive.addClass('last-active').removeClass('active');
-	$('.thumb').removeClass('active');
-
-    imgThumb.addClass('active');
-    scrollThumbnails(imgThumb);
-    image.css({opacity: 0.0})
-        .addClass('active')
-        .animate({opacity: 1.0}, 1000, function() {
-            currActive.removeClass('last-active');
-        });
-};
-
-var handleThumbClick = function(intervalId, imgId) {
-		loadClickedImage(imgId);
-};
-
-var scrollThumbnails2 = function(thumb) {
-	var x = thumb.position().left;
-	var currOffset = (x + THUMBNAIL_WIDTH) - thumb.parent().width();
-	if( currOffset > 0 ) {
-		var first = $('.thumb').first();
-		var offset = parseInt(first.css('margin-left'), 10) - currOffset;
-		first.animate({marginLeft: offset}, 1000);
-		console.log('Need to slide left -- offset = ' + offset);
-	}
-	console.log("x: " + x);
-	console.log('currOffset: ', currOffset);
-};
-
-var scrollThumbnails = function(thumb) {
-	var x = thumb.position().left + parseInt(thumb.css('margin-left'), 10);
-
-	if(x < 0) {
-		var first = $('.thumb').first();
-		var offset = parseInt(first.css('margin-left'), 10) - x;
-		first.animate({marginLeft: offset}, 1000);
-	} else {
-		x = thumb.position().left;
-		var currOffset = (x + THUMBNAIL_WIDTH) - thumb.parent().width();
-		if( currOffset > 0 ) {
-			var first = $('.thumb').first();
-			var offset = parseInt(first.css('margin-left'), 10) - currOffset;
-			first.animate({marginLeft: offset}, 1000);
-			console.log('Need to slide left -- offset = ' + offset);
-		}
-	}
-};
-
 $(function() {
-	$('.thumb').on('click', function() {
-		handleThumbClick(tId, $(this).data('thumb-id'));
+
+	/******************************
+	* STATIC VARIABLES
+	******************************/
+	var THUMBNAIL_WIDTH = 140,
+	GALLERY = $('#slideshow');
+
+
+	/******************************
+	* EVENT LISTENERS
+	******************************/
+	GALLERY.find('.thumb').on('click', function() {
+		loadClickedImage($(this).data('thumb-id'));
 	});
-	// Handling prev and next arrow presses
-	$('#prev-btn').on('click', function() {
+	GALLERY.find('#prev-btn').on('click', function() {
 		slidePrev();
 	});
-	$('#next-btn').on('click', function() {
+	GALLERY.find('#next-btn').on('click', function() {
 		slideNext();
 	});
-	// Binding arrow keypress to navigate images
 	$(document).keydown(function(e){
 		switch(e.keyCode) {
 			case 37:
-				$('.thumb').removeClass('active');
 				slidePrev();
 				break;
 			case 39:
-				$('.thumb').removeClass('active');
 				slideNext();
 				break;
 			default:
 				break;
 		}
 	});
+
+
+	/******************************
+	* GALLERY FUNCTIONS
+	******************************/
+	var slideNext = function() {
+	
+	    var active = GALLERY.find('.img-wrapper.active');
+
+		if (active.length === 0) { 
+			active = GALLERY.find('.img-wrapper:last');
+		}
+
+	    // Setting next image & thumb properties
+	    loadNextImage(active);
+	};
+
+	var loadNextImage = function(active) {
+		var next =  active.next(".img-wrapper").length ? active.next(".img-wrapper") : GALLERY.find('.img-wrapper:first'),
+			nextThumb = GALLERY.find('[data-thumb-id="' + next.data('img-id') + '"]');
+
+		// Setting next image & thumb properties
+		GALLERY.find('.thumb').removeClass('active');
+	    nextThumb.addClass('active');
+	    active.addClass('last-active');
+
+	    // Transitioning to next image & thumbnail
+	    scrollThumbnails(nextThumb);
+	    next.css({opacity: 0.0})
+	        .addClass('active')
+	        .animate({opacity: 1.0}, 1000, function() {
+	            active.removeClass('active last-active');
+	        });
+	};
+
+	var slidePrev = function() {
+	    var active = GALLERY.find('.img-wrapper.active');
+
+	    if (active.length === 0) {
+			active = GALLERY.find('.img-wrapper:last');
+	    }
+
+	    // Setting next image & thumb properties
+	    loadPrevImage(active);
+	};
+
+	var loadPrevImage = function(active) {
+		var prev =  active.prev(".img-wrapper").length ? active.prev(".img-wrapper") : GALLERY.find('.img-wrapper:last'),
+			prevThumb = GALLERY.find('[data-thumb-id="' + prev.data('img-id') + '"]');
+
+		// Setting next image & thumb properties
+		GALLERY.find('.thumb').removeClass('active');
+		prevThumb.addClass('active');
+		active.addClass('last-active');
+
+		// Transitioning to next image & thumbnail
+		scrollThumbnails(prevThumb);
+	    prev.css({opacity: 0.0})
+	        .addClass('active')
+	        .animate({opacity: 1.0}, 1000, function() {
+	            active.removeClass('active last-active');
+	        });
+	};
+
+	var loadClickedImage = function(id) {
+		var image =  GALLERY.find('[data-img-id="' + id + '"]'),
+			imgThumb = GALLERY.find('[data-thumb-id="' + id + '"]'),
+			currActive = GALLERY.find('.img-wrapper.active');
+
+		// Setting image & thumb properties
+		GALLERY.find('.thumb').removeClass('active');
+		currActive.addClass('last-active').removeClass('active');
+		imgThumb.addClass('active');
+	    
+	    // Transitioning to image & thumbnail
+	    scrollThumbnails(imgThumb);
+	    image.css({opacity: 0.0})
+	        .addClass('active')
+	        .animate({opacity: 1.0}, 1000, function() {
+	            currActive.removeClass('last-active');
+	        });
+	};
+
+	var scrollThumbnails = function(thumb) {
+		var offset, // used for thumbnail offset
+			first,  // stores first thumbnail object
+			x = thumb.position().left + parseInt(thumb.css('margin-left'), 10);
+
+		// Checking current thumbnail offset
+		if(x < 0) {
+			first = GALLERY.find('.thumb:first');
+			offset = parseInt(first.css('margin-left'), 10) - x;
+			first.animate({
+				marginLeft: offset
+			}, 1000);
+		} else {
+			x = thumb.position().left;
+			var currOffset = ( x + THUMBNAIL_WIDTH ) - thumb.parent().width();
+			if(currOffset > 0) {
+				first = GALLERY.find('.thumb:first');
+				offset = parseInt(first.css('margin-left'), 10) - currOffset;
+				first.animate({
+					marginLeft: offset
+				}, 1000);
+			}
+		}
+	};
 }());
